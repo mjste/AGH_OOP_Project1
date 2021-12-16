1a44819afa9429bbf73afc9250fb11d7feabf41c
mjste <mj.stefanik@gmail.com>
2021-12-16 18:35:47 +0100

    Added some modified classes from oolab

---
+++ src/main/java/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    private Vector2D position;
+    private MapDirection mapDirection;
+    private int energy;
+    private int[] genes;
+
+    public Animal(Vector2D position) {
+        this.position = position;
+
+
+    }
+
+}
